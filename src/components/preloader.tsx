ef5a779098e5108f05156cbe07ac3c9941c452ed
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2026-02-11 20:48:10 +0000

    nomas que hay un problama como el contenido es grande por decirlo asi de

--- src/components/preloader.tsx
+++ src/components/preloader.tsx
@@ -31,7 +31,7 @@ export function Preloader() {
   return (
     <div
       className={cn(
-        'absolute inset-0 z-40 flex items-center justify-center bg-background transition-opacity duration-500',
+        'absolute inset-0 z-40 flex items-start justify-center pt-48 bg-background transition-opacity duration-500',
         isLoading ? 'opacity-100' : 'opacity-0 pointer-events-none'
       )}
     >
