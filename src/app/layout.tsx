bd4aa07e579256004d8f7d73dd38fce3f06507fa
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2026-02-13 15:44:20 +0000

    Agregale clarity microsoft para ver rendimiento  mi id es vgs07lu7a9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { AppHeader } from '@/components/layout/header';
 import { AppFooter } from '@/components/layout/footer';
@@ -39,6 +40,18 @@ export default function RootLayout({
           <AppFooter />
         </div>
         <Toaster />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vgs07lu7a9");
+          `}
+        </Script>
       </body>
     </html>
   );
