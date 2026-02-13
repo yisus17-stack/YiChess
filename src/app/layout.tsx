2e3be568e77cdf1fce4bf9a3175fb2e8d75622bc
Yisus <esquivelyisus17@gmail.com>
2026-02-13 08:58:39 -0600

    Microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="es" className={`${lexend.variable} light scroll-smooth`}>
       <head>
+        {/* Clarity solo en producción */}
         {process.env.NODE_ENV === 'production' && (
           <Script
             id="clarity"
@@ -34,7 +35,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               __html: `
                 (function(c,l,a,r,i,t,y){
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/TU_ID_DE_CLARITY";
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/vgs07lu7a9";
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                 })(window, document, "clarity", "script", "vgs07lu7a9");
               `,
