3af0d4219595f92e718ddca80b66abaa453e3ded
Esquivel Jesus's (Yisus) <esquiveljesuss925@gmail.com>
2026-02-13 15:56:32 +0000

    en claruty no me registra

--- src/components/Clarity.tsx
+++ src/components/Clarity.tsx
@@ -3,13 +3,11 @@ import { useEffect } from 'react';
 
 export const Clarity = () => {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      console.log('Clarity script ejecutándose'); // ✅ Esto confirma que se carga
-      const script = document.createElement('script');
-      script.src = 'https://www.clarity.ms/tag/vgs07lu7a9';
-      script.async = true;
-      document.head.appendChild(script);
-    }
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })((window as any), document, "clarity", "script", "vgs07lu7a9");
   }, []);
 
   return null;
