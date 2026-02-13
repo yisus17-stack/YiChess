4a2e4d8226a59231cb3a681435e35728158eac70
Yisus <esquivelyisus17@gmail.com>
2026-02-13 09:26:44 -0600

    Clarity FUNCION ACTUAL

--- src/components/Clarity.tsx
+++ src/components/Clarity.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 export const Clarity = () => {
   useEffect(() => {
     if (process.env.NODE_ENV === 'production') {
+      console.log('Clarity script ejecutándose'); // ✅ Esto confirma que se carga
       const script = document.createElement('script');
       script.src = 'https://www.clarity.ms/tag/vgs07lu7a9';
       script.async = true;
